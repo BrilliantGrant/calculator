b4b5b08a8339e37fc0a1d4921a1a272f427af8de
brilliant kaimba <briegrant416@gmail.com>
2017-10-17 12:12:12 +0300

    add divide to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -25,8 +25,8 @@
       var result = add(number1, number2);
       alert(result);
 
-var add = function(number1, number2) {
-        return number1 + number2;
+var divide = function(number1, number2) {
+        return number1 / number2;
       };
 
       var number1 = parseInt(prompt("Enter a number:"));
