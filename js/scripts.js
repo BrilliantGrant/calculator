ce811057ae28f08662e5c75c6446d14a2d7150d6
brilliant kaimba <briegrant416@gmail.com>
2017-10-17 12:09:43 +0300

    Add multiply to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,8 +16,8 @@
       var result = add(number1, number2);
       alert(result);
       
-  var add = function(number1, number2) {
-        return number1 + number2;
+  var multiply = function(number1, number2) {
+        return number1 * number2;
       };
 
       var number1 = parseInt(prompt("Enter a number:"));
